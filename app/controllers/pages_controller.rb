6fae5b17eae94d63951a8407dd64e4f689c6a5e5
sara <sara@nanomia.cl>
2016-10-17 20:48:12 -0300

    formulario

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,15 @@
 class PagesController < ApplicationController
-  def paginax
+  def paginax	
+  	
   end
+
+   def guardar
+   	@name = params[:nombre]
+  	@edad = params[:edad]
+  	@mail = params[:mail]
+
+  	Dato.create(name: @name, edad: @edad,  mail: @mail)
+  	redirect_to datos_guardar_path 
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'datos/guardar'
+  post 'datos/guardar'
+
   get 'pages/paginax'
 
   root 'pages#paginax'
